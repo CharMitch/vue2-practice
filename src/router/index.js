328caf68f2a342e47c30c8322764e02b5d5c3544
scoffin-te <sondra.coffin@techelevator.com>
2023-08-29 15:52:39 -0400

    Continue removing authentication code and references

--- src/router/index.js
+++ src/router/index.js
@@ -7,10 +7,6 @@ Vue.use(Router)
 /**
  * The Vue Router is used to "direct" the browser to render a specific view component
  * inside of App.vue depending on the URL.
- *
- * It also is used to detect whether or not a route requires the user to have first authenticated.
- * If the user has not yet authenticated (and needs to) they are redirected to /login
- * If they have (or don't need to) they're allowed to go about their way.
  */
 
 const router = new Router({
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,13 +1,6 @@
-import axios from 'axios';
+//import axios from 'axios';
 
 export default {
 
-  login(user) {
-    return axios.post('/login', user)
-  },
-
-  register(user) {
-    return axios.post('/register', user)
-  }
 
 }
